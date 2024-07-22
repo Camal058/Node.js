191f3f90cebdb6f187de341be7d77712dbec231e
Camal <87723982+Camal058@users.noreply.github.com>
2024-07-22 18:27:00 +0400

    Hw 6 Add(Transform filter)

---
+++ HW6/app.js
@@ -0,0 +1,28 @@
+const { Transform } = require('stream');
+
+const array = [
+    { key: null, name: 'Camal' },
+    { key: 1, name: 'Kerim' },
+    { key: null, name: 'Miri' },
+    { key: 2, name: 'Tural' }
+];
+
+const filterFunc = new Transform({
+    objectMode: true,
+    transform(chunk, encoding, callback) {
+        if (chunk.key !== null) {
+            this.push(chunk);
+        }
+        callback();
+    }
+});
+
+array.forEach((element) => {
+    filteringFunc.write(element);
+});
+
+filterFunc.end();
+
+filterFunc.on('data', (chunk) => {
+    console.log(chunk);
+});
